3b17efd734043d9c896ba268a7f6c5470e139b16
julian <julian.limburg@gmx.de>
2024-04-21 13:58:54 +0200

    working selenium

--- bot.py
+++ bot.py
@@ -1,17 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 # Path to the WebDriver executable (e.g., chromedriver)
-webdriver_path = '/path/to/chromedriver'
+webdriver_path = '/home/julian/.cargo/bin/geckodriver'
 
 # Initialize the WebDriver (in this example, using Chrome)
-driver = webdriver.Chrome(executable_path=webdriver_path)
+driver = webdriver.Firefox()
 
 # Open a website
 driver.get('https://fill.dev/form/login-simple')
 
 # Find an element by its ID and type text into an input field
-input_element = driver.find_element_by_id('username')
+input_element = driver.find_element(By.ID, 'username')
 input_element.send_keys('Python')
 
 # Submit the form
