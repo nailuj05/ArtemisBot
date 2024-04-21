1233f025d30367af87b055b2560e21c0db2c8bcf
julian <julian.limburg@gmx.de>
2024-04-21 15:41:21 +0200

    Login and Arg Parser

--- bot.py
+++ bot.py
@@ -1,11 +1,23 @@
 import configparser
-
+import argparse
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+
+# ----------- ARGS -------------
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Process command line arguments')
+    parser.add_argument('repo_path', type=str, help='Path to the repository (locally)')
+    parser.add_argument('artemis_url', type=str, help='URL of the Artemis server')
+    parser.add_argument('-p', type=int, default=100, help='Desired percentage (default: 100%)')
+    return parser.parse_args()
+
+args = parse_arguments()
+
+# ---------- CONFIG ------------
 
 config = configparser.ConfigParser()
 config.read("config.ini")
@@ -37,8 +49,11 @@ username.send_keys(config["Auth"]["Username"])
 password = driver.find_element(By.ID, 'password')
 password.send_keys(config["Auth"]["Password"])
 
-# Submit the form
-# input_element.send_keys(Keys.ENTER)
+remember = driver.find_element(By.ID, "rememberMe")
+remember.click()
+
+login = driver.find_element(By.ID, "login-button")
+login.click()
 
 # Wait for the page to load (you may need to adjust the wait time)
 driver.implicitly_wait(1000)
